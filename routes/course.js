5e6f7e344664061bc0852a2d0f3e2452b4242cad
Devendra Mehta <devendramehta133@gmail.com>
2025-07-04 06:21:55 +0000

    Implement course routes

--- routes/course.js
+++ routes/course.js
@@ -1,16 +1,54 @@
 const { Router } = require('express');
 const courseRouter = Router();
+const { userAuth } = require('../middlewares/userAuth');
+const { CourseModel, PurchaseModel } = require('../db');
+const { purchaseValidationSchema } = require('../validation');
 
+// purchase a course
+courseRouter.post('/purchase', userAuth, async (req, res)=>{
+    try {
 
-courseRouter.post('/purchase', (req, res)=>{
-    res.json({
-        message: "Course purchase endpoint"
-    })
+        const userId = req.userId;
+        const courseId = req.body.courseId;
+
+        const validationResult = purchaseValidationSchema.safeParse({
+            userId,
+            courseId
+        });
+
+        if(!validationResult.success){
+            return res.status(400).json({
+                message: "Invalid course ID. Please provide a correct course ID."
+            })
+        }
+
+        const validatedData = validationResult.data;
+
+        //TODO in future: payment logic
+
+        const purchase = await PurchaseModel.create({
+            userId: validatedData.userId,
+            courseId: validatedData.courseId
+        });
+
+        return res.status(200).json({
+            message: "You have succesfully purchased the course."
+        })
+
+    } catch (err) {
+        console.error("Error while purchasing a course: " + err);
+        return res.status(500).json({
+            message: "An internal server error occurred while purchasing a course. Please try again."
+        })
+    }
 })
 
-courseRouter.get('/preview', (req, res)=>{
-    res.json({
-        message: "Course preview endpoint"
+// preview all available/offered courses
+courseRouter.get('/preview', async (req, res)=>{
+    const courses = await CourseModel.find({});
+
+    return res.status(200).json({
+        courses
     })
 })
 
--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const userRouter = Router();
 const { userAuth } = require('../middlewares/userAuth');
 const bcrypt = require('bcrypt');
 const { UserModel, PurchaseModel, CourseModel } = require("../db");
-const { userSignupValidationSchema, userSigninValidationSchema, purchaseValidationSchema } = require('../validation');
+const { userSignupValidationSchema, userSigninValidationSchema } = require('../validation');
 const jwt = require('jsonwebtoken');
 
 userRouter.post('/signup', async (req, res)=>{
@@ -134,46 +134,6 @@ userRouter.post('/signin', async (req, res)=>{
         // (e.g. issues with bcrypt hashing, jwt signing, mongoose database connection etc.)
         return res.status(500).json({
             message: "An internal server error occurred during signin. Please try again later."
-        })
-    }
-})
-
-
-// purchase a course
-userRouter.post('/purchase', userAuth, async (req, res)=>{
-    try {
-
-        const userId = req.userId;
-        const courseId = req.body.courseId;
-
-        const validationResult = purchaseValidationSchema.safeParse({
-            userId,
-            courseId
-        });
-
-        if(!validationResult.success){
-            return res.status(400).json({
-                message: "Invalid course ID. Please provide a correct course ID."
-            })
-        }
-
-        const validatedData = validationResult.data;
-
-        //TODO in future: payment logic
-
-        const purchase = await PurchaseModel.create({
-            userId: validatedData.userId,
-            courseId: validatedData.courseId
-        });
-
-        return res.status(200).json({
-            message: "You have succesfully purchased the course."
-        })
-
-    } catch (err) {
-        console.error("Error while purchasing a course: " + err);
-        return res.status(500).json({
-            message: "An internal server error occurred while purchasing a course. Please try again."
         })
     }
 })
